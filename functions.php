32689709016b94f1cf649702b04a5de1a0b3f4bf
Bl4ckf4ll <dw_blackfall@hotmail.com>
2016-08-16 21:50:54 +0000

    added functions for images sizes

--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
 function the_theme_setup () {
     add_theme_support('post-thumbnails');
+    add_image_size('small', 520, 293, true);
+    add_image_size('medium', 820, 461, true);
+    add_image_size('large', 1280, 720, true);
+    add_image_size('extra-large', 1920, 1080, true);
+    
+    add_theme_support('sidebar');
+    
+    register_sidebar(array(
+        'name'          => __( 'Home Sidebar', 'the_theme' ),
+        'id'            => 'home-sidebar',
+        'description'   => 'Sidebar on the homepage',
+        'class'         => 'sidebar col-xs-4',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>'
+        ));
 }
 
 add_action('init', 'the_theme_setup');
--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php get_header() ?>
-<div class="post-wrapper container-fluid">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<!-- post -->
-	<article class="post">
-		<div class="col-xs-4">
-			<img src="<?= the_post_thumbnail_url(); ?>" class="img-responsive">
-		</div>
-		<div class="col-xs-8">
-			<div class="post-title">
-				<h2><?php the_title(); ?></h2>
-			</div>
-			<?php the_excerpt(); ?>
-		</div>
-	</article>
-	<?php endwhile; ?>
-	<!-- post navigation -->
-	<?php else: ?>
-	<!-- no posts found -->
-	<?php endif; ?>
+<div class="content-wrapper container-fluid">
+    <div class="post-wrapper col-xs-8">
+    	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    	<!-- post -->
+    	<article <?= post_class() ?>>
+    		<div class="col-xs-5 post-thumbnail">
+    		    <picture>
+    			    <img src="<?= the_post_thumbnail_url('medium'); ?>" class="img-responsive" alt="">
+    		    </picture>
+    		</div>
+    		<div class="col-xs-7">
+    			<div class="post-title">
+    				<h2><?php the_title(); ?></h2>
+    			</div>
+    			<?php the_excerpt(); ?>
+    		</div>
+    	</article>
+    	<?php endwhile; ?>
+    	<!-- post navigation -->
+    	<?php else: ?>
+    	<!-- no posts found -->
+    	<?php endif; ?>
+    </div>
+    <div class="sidebar col-xs-4 home-sidebar">
+        <?php dynamic_sidebar('home-sidebar'); ?>
+    </div>
 </div>
 <?php get_footer() ?>
